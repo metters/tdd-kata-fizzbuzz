0b69d32ea96f9884f153bafc233bf21b12a8d438
metters <matthias.streidel@senacor.com>
2022-02-05 20:42:52 +0100

    REFACTOR Remove duplicate test cases

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -6,13 +6,8 @@ class FizzBuzzTest {
     private val sut = FizzBuzz()
 
     @Test
-    fun `that returns all numbers from 1 to 10 including 'Fizz' and 'Buzz'`() {
-        assertThat(sut.fizzBuzz(10))
-            .isEqualTo("[1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz]")
-    }
-
-    @Test
-    fun `that numbers divisible by 15 are converted to 'FizzBuzz`() {
-        assertThat(sut.fizzBuzz(15)).endsWith("FizzBuzz]")
+    fun `that returns all numbers from 1 to 15 including 'Fizz', 'Buzz', and 'FizzBuzz`() {
+        assertThat(sut.fizzBuzz(15))
+            .isEqualTo("[1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz]")
     }
 }
