f0b32306ce11bf6a6294883062aa7240c24e11db
metters <matthias.streidel@senacor.com>
2022-02-05 19:53:48 +0100

    RED Return '1' as string

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -1,5 +1,12 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class FizzBuzzTest {
 
     private val sut = FizzBuzz()
 
+    @Test
+    fun `that returns the input`() {
+        assertThat(sut.fizzBuzz(1)).isEqualTo("1")
+    }
 }
